a81b636f3aa8ee305ee482266a40f1d83f95a9e4
Hassaan Ahmed <hassaan.brix@gmail.com>
2016-12-15 21:29:12 +0100

    Add route and http content tests

--- ValidationPipeline.LogStorage.Tests/LogsControllerTests.cs
+++ ValidationPipeline.LogStorage.Tests/LogsControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
@@ -22,13 +23,63 @@ namespace ValidationPipeline.LogStorage.Tests
         }
 
         [Fact]
+        public async Task Upload_NoFileName_ReturnsNotFound()
+        {
+            // Act
+            var response = await _client.PostAsync("/api/logs/", new StreamContent(Stream.Null));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Upload_FileNameWithoutZipExtension_ReturnsNotFound()
+        {
+            // Act
+            var response = await _client.PostAsync("/api/logs/filename", new StreamContent(Stream.Null));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
         public async Task Upload_EmptyBody_ReturnsBadRequest()
         {
             // Act
-            var response = await _client.PostAsync("/api/logs", new StreamContent(Stream.Null));
+            var response = await _client.PostAsync("/api/logs/filename.zip", new StreamContent(Stream.Null));
 
             // Assert
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Upload_NoContentType_ReturnsUnsupportedMediaType()
+        {
+            // Arrange
+            const string fileName = "20161215.zip";
+            var stream = File.Open($"TestData/{fileName}", FileMode.Open, FileAccess.Read);
+
+            // Act
+            var response = await _client.PostAsync($"/api/logs/{fileName}", new StreamContent(stream));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Upload_NonZipContentType_ReturnsUnsupportedMediaType()
+        {
+            // Arrange
+            const string fileName = "20161215.zip";
+            var stream = File.Open($"TestData/{fileName}", FileMode.Open, FileAccess.Read);
+            var streamContent = new StreamContent(stream);
+            streamContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/txt");
+
+            // Act
+            var response = await _client.PostAsync($"/api/logs/{fileName}", streamContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
         }
     }
 }
--- ValidationPipeline.LogStorage/Controllers/LogsController.cs
+++ ValidationPipeline.LogStorage/Controllers/LogsController.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ValidationPipeline.LogStorage.Controllers
 {
     [Route("api/[controller]")]
     public class LogsController : Controller
     {
-        [HttpPost]
-        public IActionResult Upload()
+        private const string ZipContentType = "application/zip";
+
+        private bool IsContentTypeSupportedForUpload => !string.IsNullOrWhiteSpace(Request.ContentType) &&
+                                                        Request.ContentType.Equals(ZipContentType,
+                                                            StringComparison.OrdinalIgnoreCase);
+
+
+        [HttpPost("{zipFileName}.zip")]
+        public IActionResult Upload(string zipFileName)
         {
+            if (Request.Body.Length == 0)
+                return BadRequest();
+
+            if (!IsContentTypeSupportedForUpload)
+                return new UnsupportedMediaTypeResult();
+
             return Ok();
         }
 
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        [HttpGet("{zipFileName}/{fileName}")]
+        public IActionResult Get(string zipFileName, string fileName)
         {
             return Ok();
         }
